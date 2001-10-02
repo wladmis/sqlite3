b784863a7d5fdbf42f27c6a97b0a24cd4191e24b
D. Richard Hipp <drh@hwaci.com>
2001-10-02 13:01:49 +0000

    Remove C++ comments from btree.c.

--- src/btree.c
+++ src/btree.c
@@ -2505,13 +2505,13 @@ Pager *sqliteBtreePager(Btree *pBt){
 */
 typedef struct SanityCheck SanityCheck;
 struct SanityCheck {
-  Btree *pBt;    // The tree being checked out
-  Pager *pPager; // The associated pager.  Also accessible by pBt->pPager
-  int nPage;     // Number of pages in the database
-  int *anRef;    // Number of times each page is referenced
-  int nTreePage; // Number of BTree pages
-  int nByte;     // Number of bytes of data stored on BTree pages
-  char *zErrMsg; // An error message.  NULL of no errors seen.
+  Btree *pBt;    /* The tree being checked out */
+  Pager *pPager; /* The associated pager.  Also accessible by pBt->pPager */
+  int nPage;     /* Number of pages in the database */
+  int *anRef;    /* Number of times each page is referenced */
+  int nTreePage; /* Number of BTree pages */
+  int nByte;     /* Number of bytes of data stored on BTree pages */
+  char *zErrMsg; /* An error message.  NULL of no errors seen. */
 };
 
 /*
