813fea68195cf3f6ecfae0c691a93f4728bbb7a8
D. Richard Hipp <drh@hwaci.com>
2001-12-31 02:48:51 +0000

    Comment changes.  Change the use of BTree so that either the key is an integer or the data is empty.

--- src/sqliteInt.h
+++ src/sqliteInt.h
@@ -230,7 +230,7 @@ struct Table {
   char *zName;     /* Name of the table */
   int nCol;        /* Number of columns in this table */
   Column *aCol;    /* Information about each column */
-  int iPKey;       /* Use this column as the record-number for each row */
+  int iPKey;       /* If not less then 0, use aCol[iPKey] as the primary key */
   Index *pIndex;   /* List of SQL indexes on this table. */
   int tnum;        /* Page containing root for this table */
   u8 readOnly;     /* True if this table should not be written by the user */
