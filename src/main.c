f73a38a42619a9e9a3a1dbd30993f4007e432738
D. Richard Hipp <drh@hwaci.com>
2003-07-27 17:26:23 +0000

    Make sure the schema loader callback can handle EMPTY_RESULT_CALLBACKS being on.  Ticket #406.

--- src/main.c
+++ src/main.c
@@ -58,6 +58,7 @@ int sqliteInitCallback(void *pInit, int argc, char **argv, char **azColName){
   int nErr = 0;
 
   assert( argc==5 );
+  if( argv==0 ) return 0;   /* Might happen if EMPTY_RESULT_CALLBACKS are on */
   if( argv[0]==0 ){
     corruptSchema(pData);
     return 1;
