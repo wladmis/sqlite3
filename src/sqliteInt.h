5a285d44d4230118325329aa03c23dd562720fec
D. Richard Hipp <drh@hwaci.com>
2003-11-11 23:30:36 +0000

    Fix a typo in a #define that was commented out.  No impact to working code. Ticket #476.

--- src/sqliteInt.h
+++ src/sqliteInt.h
@@ -87,7 +87,7 @@
 /* #define SQLITE_OMIT_AUTHORIZATION  1 */
 /* #define SQLITE_OMIT_INMEMORYDB     1 */
 /* #define SQLITE_OMIT_VACUUM         1 */
-/* #define SQLITE_OMIT_TIMEDATE_FUNCS 1 */
+/* #define SQLITE_OMIT_DATETIME_FUNCS 1 */
 /* #define SQLITE_OMIT_PROGRESS_CALLBACK 1 */
 
 /*
