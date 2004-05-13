6332700c75d4ede113238a0c61ab877e994d883b
Dan Kennedy <dan.kennedy@foxboro.com.au>
2004-05-13 05:16:16 +0000

    Manifest types in indices. At the moment indices use manifest typing, but some other parts of the SQL engine do not, which can lead to some strange results.

--- src/vdbe.h
+++ src/vdbe.h
@@ -108,7 +108,6 @@ void sqlite3VdbeTrace(Vdbe*,FILE*);
 void sqlite3VdbeCompressSpace(Vdbe*,int);
 int sqlite3VdbeReset(Vdbe*,char **);
 int sqliteVdbeSetVariables(Vdbe*,int,const char**);
-int sqlite3VdbeKeyCompare(void*,int,const unsigned char*,int,
-    const unsigned char*);
+int sqlite3VdbeKeyCompare(void*,int,const void*,int, const void*);
 
 #endif
