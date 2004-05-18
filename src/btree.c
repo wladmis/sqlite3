8dc61985c7d90faad3400f4abca05a06fc0bc97d
D. Richard Hipp <drh@hwaci.com>
2004-05-18 12:50:17 +0000

    Additional debugging output from btree.c

--- src/btree.c
+++ src/btree.c
@@ -3191,14 +3191,14 @@ static int balance(MemPage *pPage){
       apNew[minI] = pT;
     }
   }
-  TRACE(("BALANCE: old: %d %d %d  new: %d %d %d %d\n",
+  TRACE(("BALANCE: old: %d %d %d  new: %d(%d) %d(%d) %d(%d) %d(%d)\n",
     pgnoOld[0], 
     nOld>=2 ? pgnoOld[1] : 0,
     nOld>=3 ? pgnoOld[2] : 0,
-    pgnoNew[0],
-    nNew>=2 ? pgnoNew[1] : 0,
-    nNew>=3 ? pgnoNew[2] : 0,
-    nNew>=4 ? pgnoNew[3] : 0));
+    pgnoNew[0], szNew[0],
+    nNew>=2 ? pgnoNew[1] : 0, nNew>=2 ? szNew[1] : 0,
+    nNew>=3 ? pgnoNew[2] : 0, nNew>=3 ? szNew[2] : 0,
+    nNew>=4 ? pgnoNew[3] : 0, nNew>=4 ? szNew[3] : 0));
 
 
   /*
