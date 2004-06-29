8af2e0e72572e98c873b7e4d8ce3171f045826cd
Dan Kennedy <dan.kennedy@foxboro.com.au>
2004-06-29 13:18:24 +0000

    Test cases intended to improve coverage of main.c.

--- src/main.c
+++ src/main.c
@@ -894,7 +894,10 @@ const void *sqlite3_errmsg16(sqlite3 *db){
   };
 
   if( db && db->pErr ){
-    if( db->magic==SQLITE_MAGIC_ERROR ){
+    if( db->magic!=SQLITE_MAGIC_OPEN && 
+        db->magic!=SQLITE_MAGIC_BUSY &&
+        db->magic!=SQLITE_MAGIC_CLOSED 
+    ){
       return (void *)(&misuseBe[SQLITE_UTF16NATIVE==SQLITE_UTF16LE?1:0]);
     }
     if( !sqlite3_value_text16(db->pErr) ){
@@ -1277,6 +1280,7 @@ int sqlite3_create_collation(
   }else{
     pColl->xCmp = xCompare;
     pColl->pUser = pCtx;
+    pColl->enc = enc;
   }
   sqlite3Error(db, rc, 0);
   return rc;
