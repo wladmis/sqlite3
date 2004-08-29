c3895d648f72157788b39c9a3c81fdcb987c49d9
D. Richard Hipp <drh@hwaci.com>
2004-08-29 23:42:14 +0000

    Change the name of the global variable to sqlite3_temp_directory to avoid a naming conflict with version 2.8.

--- src/test1.c
+++ src/test1.c
@@ -2533,6 +2533,6 @@ int Sqlitetest1_Init(Tcl_Interp *interp){
   Tcl_LinkVar(interp, "sqlite_static_bind_value",
       (char*)&sqlite_static_bind_value, TCL_LINK_STRING);
   Tcl_LinkVar(interp, "sqlite_temp_directory",
-      (char*)&sqlite_temp_directory, TCL_LINK_STRING);
+      (char*)&sqlite3_temp_directory, TCL_LINK_STRING);
   return TCL_OK;
 }
