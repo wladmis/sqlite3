a8af6a9c5fa07807a814a2e33264e6cbfcbe593b
D. Richard Hipp <drh@hwaci.com>
2004-11-11 19:32:40 +0000

    Website updates.

--- www/download.tcl
+++ www/download.tcl
@@ -12,6 +12,7 @@ puts {
 
 proc Product {pattern desc} {
   regsub VERSION $pattern {([0-9a-z._]+)} p2
+  set p2 [string map {* .*} $p2]
   regsub VERSION $pattern {*} p3
   set flist [glob -nocomplain $p3]
   foreach file [lsort -dict $flist] {
