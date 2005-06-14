cfe19c7b21b31c88a3e739dbeffbf1a8042102d8
D. Richard Hipp <drh@hwaci.com>
2005-06-14 02:12:46 +0000

    Do not record existance the sqlite_sequence table until it is actually created.  Ticket #1283.

--- src/build.c
+++ src/build.c
@@ -745,7 +745,7 @@ void sqlite3StartTable(
   ** so that INSERT can find the table easily.
   */
 #ifndef SQLITE_OMIT_AUTOINCREMENT
-  if( strcmp(zName, "sqlite_sequence")==0 ){
+  if( !pParse->nested && strcmp(zName, "sqlite_sequence")==0 ){
     db->aDb[iDb].pSeqTab = pTable;
   }
 #endif
