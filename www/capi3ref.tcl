6cdca81d3239438bd0a783834f5116ade56e2317
D. Richard Hipp <drh@hwaci.com>
2005-07-22 22:53:16 +0000

    Correct the documentation to show that sqlite3_column_text() returns a NULL pointer (not an empty string) when the column value is NULL. The same goes for sqlite3_column_blob().  Ticket #1334.

--- www/capi3ref.tcl
+++ www/capi3ref.tcl
@@ -322,8 +322,8 @@ int sqlite3_column_type(sqlite3_stmt*, int iCol);
 <tr><th>Internal Type</th><th>Requested Type</th><th>Conversion</th></tr>
 <tr><td> NULL    </td><td> INTEGER</td><td>Result is 0</td></tr>
 <tr><td> NULL </td><td>    FLOAT </td><td> Result is 0.0</td></tr>
-<tr><td> NULL </td><td>    TEXT </td><td>  Result is an empty string</td></tr>
-<tr><td> NULL </td><td>    BLOB </td><td>  Result is a zero-length BLOB</td></tr>
+<tr><td> NULL </td><td>    TEXT </td><td>  Result is NULL pointer</td></tr>
+<tr><td> NULL </td><td>    BLOB </td><td>  Result is NULL pointer</td></tr>
 <tr><td> INTEGER </td><td> FLOAT </td><td> Convert from integer to float</td></tr>
 <tr><td> INTEGER </td><td> TEXT </td><td>  ASCII rendering of the integer</td></tr>
 <tr><td> INTEGER </td><td> BLOB </td><td>  Same as for INTEGER->TEXT</td></tr>
