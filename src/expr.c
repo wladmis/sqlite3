d82a32edf0b779084e14721a5d3494e63492a46d
D. Richard Hipp <drh@hwaci.com>
2005-08-24 16:54:05 +0000

    Remove a stray C++ style comment causing problems for some compilers.

--- src/expr.c
+++ src/expr.c
@@ -1741,7 +1741,6 @@ void sqlite3ExprCode(Parse *pParse, Expr *pExpr){
          sqlite3DequoteExpr(pExpr);
          sqlite3VdbeOp3(v, OP_Halt, SQLITE_CONSTRAINT, pExpr->iColumn,
                         pExpr->token.z, pExpr->token.n);
-//         sqlite3VdbeDequoteP3(v, -1);
       } else {
          assert( pExpr->iColumn == OE_Ignore );
          sqlite3VdbeAddOp(v, OP_ContextPop, 0, 0);
