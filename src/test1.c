c6547ba41674577a9bebf70eeb9bd3bb49aecee8
Dan Kennedy <dan.kennedy@foxboro.com.au>
2005-11-25 09:01:24 +0000

    If SQLITE_DISABLE_LFS is defined, omit the bigfile tests.

--- src/test1.c
+++ src/test1.c
@@ -2823,6 +2823,12 @@ static void set_options(Tcl_Interp *interp){
   Tcl_SetVar2(interp, "sqlite_options","casesensitivelike","0",TCL_GLOBAL_ONLY);
 #endif
 
+#ifdef SQLITE_DISABLE_LFS
+  Tcl_SetVar2(interp, "sqlite_options", "lfs", "0", TCL_GLOBAL_ONLY);
+#else
+  Tcl_SetVar2(interp, "sqlite_options", "lfs", "1", TCL_GLOBAL_ONLY);
+#endif
+
 #ifdef SQLITE_OMIT_ALTERTABLE
   Tcl_SetVar2(interp, "sqlite_options", "altertable", "0", TCL_GLOBAL_ONLY);
 #else
