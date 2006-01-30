aa1162a41d688399a5a9e11a7fa4c8f24184ac77
D. Richard Hipp <drh@hwaci.com>
2006-01-30 16:20:30 +0000

    Documentation updates in preparation for the release of 3.3.3 stable.

--- www/docs.tcl
+++ www/docs.tcl
@@ -41,6 +41,11 @@ doc {SQL Syntax} {lang.html} {
 doc {Version 3 C/C++ API<br>Reference} {capi3ref.html} {
   This document describes each API function separately.
 }
+doc {Sharing Cache Mode} {sharedcache.html} {
+  Version 3.3.0 and later supports the ability for two or more
+  database connections to share the same page and schema cache.
+  This feature is useful for certain specialized applications.
+}
 doc {Tcl API} {tclsqlite.html} {
   A description of the TCL interface bindings for SQLite.
 }
