04bd20be958949dd2669c1fc80acd6dd9dda3fd2
D. Richard Hipp <drh@hwaci.com>
2006-04-07 13:26:43 +0000

    Avoid the use of atof().  Ticket #1756.

--- src/func.c
+++ src/func.c
@@ -204,7 +204,8 @@ static void roundFunc(sqlite3_context *context, int argc, sqlite3_value **argv){
   if( sqlite3_value_type(argv[0])==SQLITE_NULL ) return;
   r = sqlite3_value_double(argv[0]);
   sqlite3_snprintf(sizeof(zBuf),zBuf,"%.*f",n,r);
-  sqlite3_result_double(context, atof(zBuf));
+  sqlite3AtoF(zBuf, &r);
+  sqlite3_result_double(context, r);
 }
 
 /*
