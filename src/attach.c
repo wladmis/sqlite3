db2a7cb2ada7b6123b2e3734ec95df160b998a5c
D. Richard Hipp <drh@hwaci.com>
2006-04-10 13:37:47 +0000

    Follow-up to check-in [3164].  Make sure SQLITE_NOMEM is returned after a memory allocation failure.  It is not sufficent to return an "out of memory" error message.  The return code needs to be SQLITE_NOMEM.

--- src/attach.c
+++ src/attach.c
@@ -186,7 +186,7 @@ static void attachFunc(
     sqlite3ResetInternalSchema(db, 0);
     db->nDb = iDb;
     if( rc==SQLITE_NOMEM ){
-      sqlite3MallocFailed();
+      if( !sqlite3MallocFailed() ) sqlite3FailedMalloc();
       sqlite3_snprintf(127, zErr, "out of memory");
     }else{
       sqlite3_snprintf(127, zErr, "unable to open database: %s", zFile);
