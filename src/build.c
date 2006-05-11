a265d743929a499cdc7f60f8c5ed48bba393cbfb
D. Richard Hipp <drh@hwaci.com>
2006-05-11 23:14:59 +0000

    Adjust the default weights on indices so that an index that matches more terms of the where clause wins (by default) over one that matches fewer. Ticket #1808.

--- src/build.c
+++ src/build.c
@@ -2638,8 +2638,12 @@ void sqlite3DefaultRowEst(Index *pIdx){
   int i;
   assert( a!=0 );
   a[0] = 1000000;
-  for(i=pIdx->nColumn; i>=1; i--){
-    a[i] = 10;
+  for(i=pIdx->nColumn; i>=5; i--){
+    a[i] = 5;
+  }
+  while( i>=1 ){
+    a[i] = 11 - i;
+    i--;
   }
   if( pIdx->onError!=OE_None ){
     a[pIdx->nColumn] = 1;
