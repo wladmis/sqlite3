5edf564d2f26cb92ee9faf04702465b77a2daa23
D. Richard Hipp <drh@hwaci.com>
2006-07-26 14:57:30 +0000

    Fix an overzealous assert() in the UTF-16 to UTF-8 converter.

--- src/utf.c
+++ src/utf.c
@@ -462,8 +462,8 @@ char *sqlite3utf16to8(const void *z, int nByte){
   memset(&m, 0, sizeof(m));
   sqlite3VdbeMemSetStr(&m, z, nByte, SQLITE_UTF16NATIVE, SQLITE_STATIC);
   sqlite3VdbeChangeEncoding(&m, SQLITE_UTF8);
-  assert( m.flags & MEM_Term );
-  assert( m.flags & MEM_Str );
+  assert( (m.flags & MEM_Term)!=0 || sqlite3MallocFailed() );
+  assert( (m.flags & MEM_Str)!=0 || sqlite3MallocFailed() );
   return (m.flags & MEM_Dyn)!=0 ? m.z : sqliteStrDup(m.z);
 }
 
