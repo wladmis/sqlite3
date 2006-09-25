3a217bd6669ca37f0b1ddeb1717fec4247f28a75
D. Richard Hipp <drh@hwaci.com>
2006-09-25 18:05:04 +0000

    CVS malfunctioned on the previous check-in.  Make sure the revision to date.c was received.  Ticket #1991.

--- src/date.c
+++ src/date.c
@@ -23,7 +23,7 @@
 ** SQLite processes all times and dates as Julian Day numbers.  The
 ** dates and times are stored as the number of days since noon
 ** in Greenwich on November 24, 4714 B.C. according to the Gregorian
-** calendar system.
+** calendar system. 
 **
 ** 1970-01-01 00:00:00 is JD 2440587.5
 ** 2000-01-01 00:00:00 is JD 2451544.5
