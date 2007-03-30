44cc5ef857fb2df15f0adadd4013d2ba0f1152ed
Dan Kennedy <dan.kennedy@foxboro.com.au>
2007-03-30 17:11:13 +0000

    Coverage tests for some pragmas.

--- src/pragma.c
+++ src/pragma.c
@@ -100,7 +100,7 @@ static int getTempStore(const char *z){
 static int invalidateTempStorage(Parse *pParse){
   sqlite3 *db = pParse->db;
   if( db->aDb[1].pBt!=0 ){
-    if( db->flags & SQLITE_InTrans ){
+    if( !db->autoCommit ){
       sqlite3ErrorMsg(pParse, "temporary storage cannot be changed "
         "from within a transaction");
       return SQLITE_ERROR;
