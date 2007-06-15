f7fee73f5ae6678000df687e7b16e76d8630135d
D. Richard Hipp <drh@hwaci.com>
2007-06-15 18:59:09 +0000

    Fix more documentation typos.  Ticket #2416.

--- www/lang.tcl
+++ www/lang.tcl
@@ -1490,7 +1490,7 @@ UTF-8 strings.</td>
 <td valign="top" align="right">zeroblob(<i>N</i>)</td>
 <td valign="top"><a name="zeroblob">
 Return a BLOB consisting of N bytes of 0x00.  SQLite
-manages these zeroblobs very efficiently.  Zeroblobs can be use to
+manages these zeroblobs very efficiently.  Zeroblobs can be used to
 reserve space for a BLOB that is later written using 
 <a href="capi3ref.html#sqlite3_blob_open">incremental BLOB I/O</a>.</td>
 </tr>
