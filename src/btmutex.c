a3c4f0c2ce35c7d2ce9a81cc82f60817dc59b3c0
D. Richard Hipp <drh@hwaci.com>
2007-08-28 16:44:20 +0000

    Fix locking bug in btshared.c.

--- src/btmutex.c
+++ src/btmutex.c
@@ -85,6 +85,7 @@ void sqlite3BtreeEnter(Btree *p){
     }
   }
   sqlite3_mutex_enter(p->pBt->mutex);
+  p->locked = 1;
   for(pLater=p->pNext; pLater; pLater=pLater->pNext){
     if( pLater->wantToLock ){
       sqlite3_mutex_enter(pLater->pBt->mutex);
@@ -168,6 +169,7 @@ void sqlite3BtreeMutexSetEnter(BtreeMutexSet *pSet){
     p->wantToLock++;
     if( !p->locked ){
       sqlite3_mutex_enter(p->pBt->mutex);
+      p->locked = 1;
     }
   }
 }
@@ -191,6 +193,7 @@ void sqlite3BtreeMutexSetLeave(BtreeMutexSet *pSet){
     p->wantToLock--;
     if( p->wantToLock==0 ){
       sqlite3_mutex_leave(p->pBt->mutex);
+      p->locked = 0;
     }
   }
 }
