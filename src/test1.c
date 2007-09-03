56c2b8a62725a6f17c8aa03184a6ea3780e7ba7b
D. Richard Hipp <drh@hwaci.com>
2007-09-03 17:02:50 +0000

    Get the io.test tests working on windows.

--- src/test1.c
+++ src/test1.c
@@ -4469,12 +4469,9 @@ int Sqlitetest1_Init(Tcl_Interp *interp){
       (char*)&sqlite3_temp_directory, TCL_LINK_STRING);
   Tcl_LinkVar(interp, "bitmask_size",
       (char*)&bitmask_size, TCL_LINK_INT|TCL_LINK_READ_ONLY);
-#if OS_UNIX
   Tcl_LinkVar(interp, "sqlite_sync_count",
       (char*)&sqlite3_sync_count, TCL_LINK_INT);
   Tcl_LinkVar(interp, "sqlite_fullsync_count",
       (char*)&sqlite3_fullsync_count, TCL_LINK_INT);
-#endif /* OS_UNIX */
-
   return TCL_OK;
 }
