628012bfaeab16d1ecf120b056fd58febc8a01b5
D. Richard Hipp <drh@hwaci.com>
2008-01-22 16:35:37 +0000

    Detect and report a particular type of database corruption that would formerly cause a segfault.

--- src/prepare.c
+++ src/prepare.c
@@ -86,6 +86,8 @@ int sqlite3InitCallback(void *pInit, int argc, char **argv, char **azColName){
       sqlite3_free(zErr);
       return 1;
     }
+  }else if( argv[2]==0 ){
+    corruptSchema(pData, 0);
   }else{
     /* If the SQL column is blank it means this is an index that
     ** was created to be the PRIMARY KEY or to fulfill a UNIQUE
