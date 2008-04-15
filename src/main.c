b3791fa4eda774a7aa0ad6d4974dbc881af6b67d
D. Richard Hipp <drh@hwaci.com>
2008-04-15 04:02:41 +0000

    Fix a bug in the RTRIM collating sequence discovered while working on condition/decision branch coverage.  Increase test coverage of the date/time functions.

--- src/main.c
+++ src/main.c
@@ -62,7 +62,7 @@ void sqlite3Coverage(int x){
 ** Return true if the buffer z[0..n-1] contains all spaces.
 */
 static int allSpaces(const char *z, int n){
-  while( n>0 && z[--n]==' ' ){}
+  while( n>0 && z[n-1]==' ' ){ n--; }
   return n==0;
 }
 
