d071bb4ad3c421a88a4cef07055c3d84ecde039e
Dan Kennedy <dan.kennedy@foxboro.com.au>
2008-05-16 15:24:58 +0000

    Check that the encoding of values passed to sqlite3_bind_value() matches that of the database.

--- src/vdbeapi.c
+++ src/vdbeapi.c
@@ -1131,6 +1131,9 @@ int sqlite3_bind_value(sqlite3_stmt *pStmt, int i, const sqlite3_value *pValue){
   rc = vdbeUnbind(p, i);
   if( rc==SQLITE_OK ){
     rc = sqlite3VdbeMemCopy(&p->aVar[i-1], pValue);
+    if( rc==SQLITE_OK ){
+      rc = sqlite3VdbeChangeEncoding(&p->aVar[i-1], ENC(p->db));
+    }
   }
   rc = sqlite3ApiExit(p->db, rc);
   sqlite3_mutex_leave(p->db->mutex);
