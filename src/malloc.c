baaac8f4b669763e5192b9410918a91c9d48c586
D. Richard Hipp <drh@hwaci.com>
2008-07-18 18:56:17 +0000

    Use the actual size of memory allocations to update the memory status counters.  Fix the roundup() function of mem3 to be much closer to the actual allocation size.  Ticket #3226.

--- src/malloc.c
+++ src/malloc.c
@@ -224,7 +224,10 @@ static int mallocWithAlarm(int n, void **pp){
     sqlite3MallocAlarm(nFull);
     p = sqlite3Config.m.xMalloc(nFull);
   }
-  if( p ) sqlite3StatusAdd(SQLITE_STATUS_MEMORY_USED, nFull);
+  if( p ){
+    nFull = sqlite3MallocSize(p);
+    sqlite3StatusAdd(SQLITE_STATUS_MEMORY_USED, nFull);
+  }
   *pp = p;
   return nFull;
 }
@@ -504,6 +507,7 @@ void *sqlite3Realloc(void *pOld, int nBytes){
         pNew = sqlite3Config.m.xRealloc(pOld, nNew);
       }
       if( pNew ){
+        nNew = sqlite3MallocSize(pNew);
         sqlite3StatusAdd(SQLITE_STATUS_MEMORY_USED, nNew-nOld);
       }
     }
