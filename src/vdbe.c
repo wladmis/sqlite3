e938ede8eaa511c75e4e32d60716612e6f9c8c0d
D. Richard Hipp <drh@hwaci.com>
2008-07-30 13:27:11 +0000

    Implicit string->numeric conversion should go to an integer value when possible.  Ticket #3257.

--- src/vdbe.c
+++ src/vdbe.c
@@ -1179,6 +1179,8 @@ case OP_Multiply:              /* same as TK_STAR, in1, in2, out3 */
 case OP_Divide:                /* same as TK_SLASH, in1, in2, out3 */
 case OP_Remainder: {           /* same as TK_REM, in1, in2, out3 */
   int flags;
+  applyNumericAffinity(pIn1);
+  applyNumericAffinity(pIn2);
   flags = pIn1->flags | pIn2->flags;
   if( (flags & MEM_Null)!=0 ) goto arithmetic_result_is_null;
   if( (pIn1->flags & pIn2->flags & MEM_Int)==MEM_Int ){
