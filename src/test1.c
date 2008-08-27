108473fd1a7e0ac3865831dbddb631acc0044cdb
D. Richard Hipp <drh@hwaci.com>
2008-08-27 15:21:34 +0000

    In the test logic: load the md5sum() SQL function as a separate autoloaded extension.

--- src/test1.c
+++ src/test1.c
@@ -929,7 +929,6 @@ static int test_create_function(
 ){
   int rc;
   sqlite3 *db;
-  extern void Md5_Register(sqlite3*);
 
   if( argc!=2 ){
     Tcl_AppendResult(interp, "wrong # args: should be \"", argv[0],
