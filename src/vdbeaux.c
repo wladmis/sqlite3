90373d66b1953938a400849231c6256d0392fd62
D. Richard Hipp <drh@hwaci.com>
2008-11-05 17:41:19 +0000

    Fix memory allocation problems when string length exceeds limits.

--- src/vdbeaux.c
+++ src/vdbeaux.c
@@ -1229,7 +1229,7 @@ int sqlite3VdbeSetColName(
   assert( p->aColName!=0 );
   pColName = &(p->aColName[idx+var*p->nResColumn]);
   rc = sqlite3VdbeMemSetStr(pColName, zName, -1, SQLITE_UTF8, xDel);
-  assert( p->db->mallocFailed || !zName || pColName->flags&MEM_Term );
+  assert( rc!=0 || !zName || (pColName->flags&MEM_Term)!=0 );
   return rc;
 }
 
