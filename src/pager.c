22d9d3c5ff02de6922305d44f8f4f45972cb613a
Dan Kennedy <dan.kennedy@foxboro.com.au>
2009-01-07 10:52:56 +0000

    Add a comment to the openSubjournal() function in pager.c.

--- src/pager.c
+++ src/pager.c
@@ -3185,6 +3185,15 @@ int sqlite3PagerUnref(DbPage *pPg){
   return SQLITE_OK;
 }
 
+/*
+** If the main journal file has already been opened, ensure that the
+** sub-journal file is open too. If the main journal is not open,
+** this function is a no-op.
+**
+** SQLITE_OK is returned if everything goes according to plan. An 
+** SQLITE_IOERR_XXX error code is returned if the call to 
+** sqlite3OsOpen() fails.
+*/
 static int openSubJournal(Pager *pPager){
   int rc = SQLITE_OK;
   if( pPager->journalOpen && !pPager->sjfd->pMethods ){
