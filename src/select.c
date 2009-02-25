bbda66a9cf7ae5ceaf5fb5942414f7045b098476
Dan Kennedy <dan.kennedy@foxboro.com.au>
2009-02-25 08:56:47 +0000

    Minor changes and coverge tests for "SELECT count(*)" optimization.

--- src/select.c
+++ src/select.c
@@ -2975,10 +2975,11 @@ static Table *isSimpleCount(Select *p, AggInfo *pAggInfo){
   ){
     return 0;
   }
-
   pTab = p->pSrc->a[0].pTab;
   pExpr = p->pEList->a[0].pExpr;
-  if( !pTab || pTab->pSelect || IsVirtual(pTab) ) return 0;
+  assert( pTab && !pTab->pSelect && pExpr );
+
+  if( IsVirtual(pTab) ) return 0;
   if( pExpr->op!=TK_AGG_FUNCTION ) return 0;
   if( (pAggInfo->aFunc[0].pFunc->flags&SQLITE_FUNC_COUNT)==0 ) return 0;
   if( pExpr->flags&EP_Distinct ) return 0;
