b9a905d8193b1b229c83d7ac1eb74e5a5abd4080
D. Richard Hipp <drh@hwaci.com>
2009-04-14 12:58:20 +0000

    Change the sqlite3_bind_value() implementation to use a default branch on the type switch so that there are no untested jumps in the switch.

--- src/vdbeapi.c
+++ src/vdbeapi.c
@@ -1153,10 +1153,6 @@ int sqlite3_bind_text16(
 int sqlite3_bind_value(sqlite3_stmt *pStmt, int i, const sqlite3_value *pValue){
   int rc;
   switch( pValue->type ){
-    case SQLITE_NULL: {
-      rc = sqlite3_bind_null(pStmt, i);
-      break;
-    }
     case SQLITE_INTEGER: {
       rc = sqlite3_bind_int64(pStmt, i, pValue->u.i);
       break;
@@ -1174,7 +1170,12 @@ int sqlite3_bind_value(sqlite3_stmt *pStmt, int i, const sqlite3_value *pValue){
       break;
     }
     case SQLITE_TEXT: {
-      rc = bindText(pStmt,i,  pValue->z, pValue->n, SQLITE_TRANSIENT, pValue->enc);
+      rc = bindText(pStmt,i,  pValue->z, pValue->n, SQLITE_TRANSIENT,
+                              pValue->enc);
+      break;
+    }
+    default: {
+      rc = sqlite3_bind_null(pStmt, i);
       break;
     }
   }
