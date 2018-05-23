f1364cb9fe7f50b4df9dc5f92988491c582550af
Vladimir D. Seleznev <vseleznv@altlinux.org>
2018-05-23 21:07:05 +0300

    https://www.sqlite.org/2018/sqlite-src-3230100 https://www.sqlite.org/2018/sqlite-doc-3230100

--- sqlite/src/expr.c
+++ sqlite/src/expr.c
@@ -5018,12 +5018,16 @@ static int impliesNotNullRow(Walker *pWalker, Expr *pExpr){
 
   if( ExprHasProperty(pExpr, EP_FromJoin) ) return WRC_Prune;
   switch( pExpr->op ){
+    case TK_ISNOT:
+    case TK_NOT:
     case TK_ISNULL:
     case TK_IS:
     case TK_OR:
     case TK_CASE:
     case TK_IN:
     case TK_FUNCTION:
+      testcase( pExpr->op==TK_ISNOT );
+      testcase( pExpr->op==TK_NOT );
       testcase( pExpr->op==TK_ISNULL );
       testcase( pExpr->op==TK_IS );
       testcase( pExpr->op==TK_OR );
@@ -5037,6 +5041,27 @@ static int impliesNotNullRow(Walker *pWalker, Expr *pExpr){
         return WRC_Abort;
       }
       return WRC_Prune;
+
+    /* Virtual tables are allowed to use constraints like x=NULL.  So
+    ** a term of the form x=y does not prove that y is not null if x
+    ** is the column of a virtual table */
+    case TK_EQ:
+    case TK_NE:
+    case TK_LT:
+    case TK_LE:
+    case TK_GT:
+    case TK_GE:
+      testcase( pExpr->op==TK_EQ );
+      testcase( pExpr->op==TK_NE );
+      testcase( pExpr->op==TK_LT );
+      testcase( pExpr->op==TK_LE );
+      testcase( pExpr->op==TK_GT );
+      testcase( pExpr->op==TK_GE );
+      if( (pExpr->pLeft->op==TK_COLUMN && IsVirtual(pExpr->pLeft->pTab))
+       || (pExpr->pRight->op==TK_COLUMN && IsVirtual(pExpr->pRight->pTab))
+      ){
+       return WRC_Prune;
+      }
     default:
       return WRC_Continue;
   }
